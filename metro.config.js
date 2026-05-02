3a2d032e4e8c59f1d90ddfe29a60d4bd4da4cd29
Zuri Luxe Developer <dev@zuriluxe.com>
2026-05-02 16:26:00 +0300

    fix: use resolveRequest to fix clerk.headless nested module path

--- metro.config.js
+++ metro.config.js
@@ -9,4 +9,21 @@ config.resolver.nodeModulesPaths = [
   path.resolve(__dirname, "node_modules/@clerk/clerk-expo/node_modules"),
 ];
 
+// Fix clerk.headless relative import inside nested node_modules
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (
+    moduleName === "../dist/clerk.headless" &&
+    context.originModulePath.includes("@clerk/clerk-js/headless")
+  ) {
+    return {
+      filePath: path.resolve(
+        __dirname,
+        "node_modules/@clerk/clerk-expo/node_modules/@clerk/clerk-js/dist/clerk.headless.js"
+      ),
+      type: "sourceFile",
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
