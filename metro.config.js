ecfb361f443db38a7f603ae9c379a9e335168de6
Zuri Luxe Developer <dev@zuriluxe.com>
2026-05-02 16:11:14 +0300

    fix: add clerk nested node_modules to Metro resolver paths

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,12 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, "node_modules"),
+  path.resolve(__dirname, "node_modules/@clerk/clerk-expo/node_modules"),
+];
 
 module.exports = withNativeWind(config, { input: "./global.css" });
